33d07b6d87b4b2e0c72b282f04f4a381a2396f83
Rodrigo Barreto <rodrigo.barreto@mall-connect.com>
2017-07-29 12:44:40 +0200

    some sampes with react

---
+++ ex_3/component.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function() {
+    return <h1>Fist component!</h1>
+}
---
+++ ex_7/component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const First = props => (
+    <h1>Primeiro Componente!</h1>
+)
+
+export const Second = props => (
+    <h1>Segundo Componente!</h1>
+)
+
+// export { Primeiro, Segundo }
